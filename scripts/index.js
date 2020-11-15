a9e624a6287ba92dc6c0b1b4ccfdd2db070c536d
Rui Hou <harryhourui@outlook.com>
2020-11-14 22:55:53 -0500

    shrink image

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,40 @@
 let imgElement = document.getElementById('imageSrc');
 let inputElement = document.getElementById('fileInput');
 let ima;
+let gray;
+let openCvReady = false;
+
+function onOpenCvReady() {
+    openCvReady = true;
+    document.getElementById('input').hidden = false;
+    document.getElementById('carver').hidden = false;
+}
+
+window.onload = function () {
+    if (openCvReady) {
+        ima = cv.imread(imgElement);
+        cv.imshow("imageCanvas", ima);
+        gray = new cv.Mat(ima.rows, ima.cols, cv.CV_8U);
+        cv.cvtColor(ima, gray, cv.COLOR_RGBA2GRAY);
+    }
+}
 
 inputElement.onchange = function() {
     imgElement.src = URL.createObjectURL(this.files[0]);
 };
 
 imgElement.onload = function() {
-    ima = cv.imread(imgElement);
-    cv.imshow("imageCanvas", ima);
-};
+    if (openCvReady) {
+        ima = cv.imread(imgElement);
+        cv.imshow("imageCanvas", ima);
+        ima = cv.imread(imgElement);
+        cv.imshow("imageCanvas", ima);
+        gray = new cv.Mat(ima.rows, ima.cols, cv.CV_8U);
+        cv.cvtColor(ima, gray, cv.COLOR_RGBA2GRAY);
+    }
+}
 
+// Write file name in file selector
 document.querySelector('.custom-file-input').addEventListener('change',function(e){
     let fileName = document.getElementById("fileInput").files[0].name;
     let nextSibling = e.target.nextElementSibling;
--- scripts/seamcarving.js
+++ scripts/seamcarving.js
@@ -1,70 +1,78 @@
 function seamCarve() {
     let image = ima.clone();
-    let gray = new cv.Mat(image.rows, image.cols, cv.CV_16S);
-    let sobelx = new cv.Mat(image.rows, image.cols, cv.CV_16S);
-    let sobely = new cv.Mat(image.rows, image.cols, cv.CV_16S);
-    let xabs = new cv.Mat(image.rows, image.cols, cv.CV_16S);
-    let yabs = new cv.Mat(image.rows, image.cols, cv.CV_16S);
-    let energy = new cv.Mat(image.rows, image.cols, cv.CV_16S);
-    cv.cvtColor(image, gray, cv.COLOR_RGBA2GRAY);
-    cv.Sobel(gray, sobelx, cv.CV_16S, 1, 0, 3);
-    cv.Sobel(gray, sobely, cv.CV_16S, 0, 1, 3);
+    let grayTemp = gray.clone();
+    let rows = image.rows;
+    let cols = image.cols;
+    let sobelx = new cv.Mat(rows, cols, cv.CV_16S);
+    let sobely = new cv.Mat(rows, cols, cv.CV_16S);
+    let xabs = new cv.Mat(rows, cols, cv.CV_16S);
+    let yabs = new cv.Mat(rows, cols, cv.CV_16S);
+    let energy = new cv.Mat(rows, cols, cv.CV_16S);
+    cv.Sobel(grayTemp, sobelx, cv.CV_16S, 1, 0, 3);
+    cv.Sobel(grayTemp, sobely, cv.CV_16S, 0, 1, 3);
     cv.convertScaleAbs(sobelx, xabs);
     cv.convertScaleAbs(sobely, yabs);
     cv.addWeighted(xabs, 0.5, yabs, 0.5, 0, energy);
-    // cv.imshow('imageCanvas', energy);
-    let ans = findSeam(energy);
-    for (let x = image.rows - 1; x >= 0; x--) {
-        let row = x;
-        let col = ans[image.rows - x - 1];
-        image.data[row * image.cols * 4 + col * 4] = 255;
-        image.data[row * image.cols * 4 + col * 4 + 1] = 0;
-        image.data[row * image.cols * 4 + col * 4 + 2] = 0;
-        image.data[row * image.cols * 4 + col * 4 + 3] = 255;
+    let seam = findSeam(energy);
+    for (let x = 0; x < image.rows; x++) {
+        let col = seam[x];
+        image.data[x * cols * 4 + col * 4    ] = 255; // R
+        image.data[x * cols * 4 + col * 4 + 1] = 0;   // G
+        image.data[x * cols * 4 + col * 4 + 2] = 0;   // B
+        image.data[x * cols * 4 + col * 4 + 3] = 255; // A
     }
     cv.imshow('imageCanvas', image);
 
-    for (let x = image.rows - 1; x >= 0; x--) {
-        let row = x;
-        let dcol = ans[image.rows - x - 1];
-        for (let y = 0; y < image.cols; y++) {
+    ima = new cv.Mat(rows, cols - 1, cv.CV_8UC4);
+    gray = new cv.Mat(rows, cols - 1, cv.CV_8U);
+    for (let x = 0; x < rows; x++) {
+        let dcol = seam[x];
+        for (let y = 0; y < cols; y++) {
             if (y !== dcol) {
                 let col = y;
                 if (y > dcol) {
                     col--;
                 }
-                ima.data[row * image.cols * 4 + col * 4] = image.data[row * image.cols * 4 + y * 4];
-                ima.data[row * image.cols * 4 + col * 4 + 1] = image.data[row * image.cols * 4 + y * 4 + 1];
-                ima.data[row * image.cols * 4 + col * 4 + 2] = image.data[row * image.cols * 4 + y * 4 + 2];
-                ima.data[row * image.cols * 4 + col * 4 + 3] = image.data[row * image.cols * 4 + y * 4 + 3];
+                ima.data[x * ima.cols * 4 + col * 4    ] = image.data[x * image.cols * 4 + y * 4    ];
+                ima.data[x * ima.cols * 4 + col * 4 + 1] = image.data[x * image.cols * 4 + y * 4 + 1];
+                ima.data[x * ima.cols * 4 + col * 4 + 2] = image.data[x * image.cols * 4 + y * 4 + 2];
+                ima.data[x * ima.cols * 4 + col * 4 + 3] = image.data[x * image.cols * 4 + y * 4 + 3];
+
+                gray.data[x * gray.cols + col] = grayTemp.data[x * grayTemp.cols + y];
             }
         }
     }
 
     image.delete();
     energy.delete();
-    gray.delete();
+    grayTemp.delete();
+    xabs.delete();
+    yabs.delete();
     sobelx.delete();
     sobely.delete();
 }
 
 function findSeam(energy){
     // DAG shortest path
-    let x_max = energy.rows;
-    let y_max = energy.cols;
-    let cost = [];
-    let prev = new Array(x_max * y_max);
-    for (let i = 0; i < y_max; i++) {
-        cost.push(energy.data[i]);
+    let t0 = performance.now();
+    let rows = energy.rows;
+    let cols = energy.cols;
+    let cost = new Int16Array(rows * cols);
+    let prev = new Int16Array(rows * cols);
+    for (let i = 0; i < cols; i++) {
+        cost[i] = energy.data[i];
     }
-    for (let i = y_max; i < x_max * y_max; i++) {
-        cost.push(32767);
+    for (let i = cols; i < rows * cols; i++) {
+        cost[i] = 32767;
     }
+
+    let t1 = performance.now();
+    // Row by row update 3 pixels right below
     let cur_index = 0;
-    for (let x = 0; x < x_max - 1; x++) {
-        for (let y = 0; y < y_max; y++) {
-            for (let next_y = Math.max(0, y - 1); next_y <= Math.min(y_max - 1, y + 1); next_y++) {
-                let next_index = (x + 1) * y_max + next_y;
+    for (let x = 0; x < rows - 1; x++) {
+        for (let y = 0; y < cols; y++) {
+            for (let next_y = Math.max(0, y - 1); next_y <= Math.min(cols - 1, y + 1); next_y++) {
+                let next_index = (x + 1) * cols + next_y;
                 let new_cost = cost[cur_index] + energy.data[next_index];
                 if (new_cost < cost[next_index]) {
                     cost[next_index] = new_cost;
@@ -74,18 +82,24 @@ function findSeam(energy){
             cur_index++;
         }
     }
+
+    let t2 = performance.now();
+    // Backtracing
     let final_cost = 32767;
     let final_col = -1;
-    for (let y = 0; y < y_max; y++) {
+    for (let y = 0; y < cols; y++) {
         if (cost[cur_index] < final_cost) {
             final_cost = cost[cur_index];
             final_col = y;
         }
         cur_index++;
     }
-    let result = [final_col];
-    for (let x = x_max - 1; x > 0; x--) {
-        result.push(prev[(x - 1) * y_max + result[result.length - 1]]);
+    let result = new Int16Array(rows);
+    result[rows - 1] = final_col;
+    for (let x = rows - 1; x > 0; x--) {
+        result[x - 1] = prev[x * cols + result[x]];
     }
+
+    let t3 = performance.now();
     return result;
 }
