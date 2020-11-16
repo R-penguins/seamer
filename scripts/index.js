0eb264f7b4b33fb958bd95ca1fe0316c6e5b8998
Rui Hou <harryhourui@outlook.com>
2020-11-16 15:13:49 -0500

    Bootstrap 5

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,8 @@ let imgElement = document.getElementById('imageSrc');
 let inputElement = document.getElementById('fileInput');
 let seamOn = document.getElementById('seamSwitch');
 let gradientOn = document.getElementById('gradientSwitch');
+let slider = document.getElementById('ratioSlider');
+let percentageOutput = document.getElementById('percentage');
 let ima;
 let ori_ima;
 let gray;
@@ -9,23 +11,19 @@ let ori_gray;
 let energy;
 let openCvReady = false;
 let sliderVal = 100;
-let slider = new Slider('#ratioSlider', {
-	formatter: function(value) {
-		return value + '%';
-	}
-});
+let ori_rows;
+let ori_cols;
 
 function onOpenCvReady() {
     cv['onRuntimeInitialized']=()=>{
         openCvReady = true;
-        document.getElementById('input').hidden = false;
-        document.getElementById('ratioSlider').hidden = false;
-        document.getElementById('seamCheckbox').hidden = false;
-        document.getElementById('gradientCheckbox').hidden = false;
+        document.getElementById('mainUI').hidden = false;
         document.getElementById('spinner').hidden = true;
         ima = cv.imread(imgElement);
         cv.imshow("imageCanvas", ima);
         ori_ima = ima.clone();
+        ori_rows = ima.rows;
+        ori_cols = ima.cols;
         gray = new cv.Mat(ima.rows, ima.cols, cv.CV_8U);
         cv.cvtColor(ima, gray, cv.COLOR_RGBA2GRAY);
         ori_gray = gray.clone();
@@ -44,16 +42,19 @@ imgElement.onload = function() {
     gray = new cv.Mat(ima.rows, ima.cols, cv.CV_8U);
     cv.cvtColor(ima, gray, cv.COLOR_RGBA2GRAY);
     ori_gray = gray.clone();
+    ori_rows = ima.rows;
+    ori_cols = ima.cols;
     energy = getEnergy();
 }
 
 function reset() {
     ima = ori_ima.clone();
     cv.imshow("imageCanvas", ima);
-    slider.refresh();
+    slider.value = 100;
     gray = ori_gray.clone();
     energy = getEnergy();
     sliderVal = 100;
+    percentageOutput.innerHTML = '100%';
 }
 
 /**
@@ -73,22 +74,16 @@ function carve (num) {
     }
 }
 
-slider.on('slideStop', function(curValue) {
-    slider.disable();
-    if (curValue > sliderVal && sliderVal < 100) {
+slider.oninput = function() {
+    slider.disabled = true;
+    percentageOutput.innerHTML = this.value + '%';
+    if (this.value > sliderVal && sliderVal < 100) {
         ima = ori_ima.clone();
         gray = ori_gray.clone();
         energy = getEnergy();
         sliderVal = 100;
     }
-    carve((sliderVal - curValue) * ima.cols / 100);
-    sliderVal = curValue;
-    slider.enable();
-});
-
-// Write file name in file selector
-document.querySelector('.custom-file-input').addEventListener('change',function(e){
-    let fileName = document.getElementById("fileInput").files[0].name;
-    let nextSibling = e.target.nextElementSibling;
-    nextSibling.innerText = fileName;
-})
+    window.setTimeout(carve((sliderVal - this.value) * ori_cols / 100));
+    sliderVal = this.value;
+    slider.disabled = false;
+};
